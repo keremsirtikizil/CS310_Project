ffb1898cef31219a52ab67a16a11bdda0bf4e4de
Kerem <159260150+keremsirtikizil@users.noreply.github.com>
2025-04-19 01:15:56 +0300

    Update add.dart

--- lib/routes/add.dart
+++ lib/routes/add.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
-
+import 'package:grocery_list/utils/appBar.dart';
+import 'package:grocery_list/utils/navbar.dart';
 class AddPage extends StatelessWidget {
   const AddPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Add Page')),
+      appBar: AppBarWithArrow(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Center(child: Text('Add Page')),
+          ElevatedButton(onPressed: () {Navigator.pushNamed(context, '/newListCreation');}, child: Text("Create new list."))
+        ],
+      ),
+      bottomNavigationBar: AppNavBar(currentIndex: 1),
+
     );
   }
 }
